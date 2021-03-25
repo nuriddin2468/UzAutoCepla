605982be24662a66d5740bc8d8a061832e63cdbf
Nuriddin <nuriddin_2468@mail.ru>
2021-03-25 10:13:27 +0500

    Mysql connections fixed

--- bot.py
+++ bot.py
@@ -1,50 +1,78 @@
 import telebot
 import helpers
 import time
-import sqlite3
+import mysql.connector
+from mysql.connector import errorcode
+import sys
+from time import sleep
 
 API_TOKEN = '1725219040:AAGFuicW0cXzJXVLLJK1qVJxO_1uBElU2Vk'
 
 bot = telebot.TeleBot(API_TOKEN)
 
 
+def getContactMarkup():
+    markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    markup.add(telebot.types.KeyboardButton('Отправить свой номер', request_contact=True))
+    return markup
+
+def getConnection():
+    try:
+        db = mysql.connector.connect(
+            host="193.23.127.34",
+            user="desmond",
+            passwd="Agent007!",
+            port="3306",
+            database="cepla"
+        )
+        return db
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+        sleep(60)
+        getConnection()
+
+
+
 def getUser(chat_id):
-    with sqlite3.connect("db.sqlite") as con:
-        cursor = con.cursor()
-        cursor.execute("SELECT * FROM users WHERE chat_id = ?", (chat_id,))
-        result = cursor.fetchone()
-        return result
+    db = getConnection()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users WHERE chat_id = %s", (chat_id, ))
+    result = cursor.fetchone()
+    return result
 
 
 def getAllUsers():
-    with sqlite3.connect("db.sqlite") as con:
-        cursor = con.cursor()
-        cursor.execute("SELECT * FROM users")
-        result = cursor.fetchall()
-        return result
+    db = getConnection()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users")
+    result = cursor.fetchall()
+    return result
 
 
 def createUser(chat_id, phone, name):
-    with sqlite3.connect("db.sqlite") as con:
-        cursor = con.cursor()
-        cursor.execute("INSERT INTO users (chat_id, phone, draft) VALUES (? , ?, ?)", (chat_id, phone, name))
-        con.commit()
+    db = getConnection()
+    cursor = db.cursor()
+    cursor.execute("INSERT INTO users (chat_id, phone, draft) VALUES (%s , %s, %s)", (chat_id, phone, name))
+    db.commit()
 
 
 @bot.message_handler(commands=['start'])
 def command_start(message):
     if getUser(message.chat.id) is None:
-        markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
-        markup.add(telebot.types.KeyboardButton('Отправить свой номер', request_contact=True))
-        bot.send_message(message.chat.id, 'Пожалуйста отправьте свой номер', reply_markup=markup)
+        bot.send_message(message.chat.id, 'Пожалуйста отправьте свой номер(Кликните по кнопке снизу!)', reply_markup=getContactMarkup())
 
 
 @bot.message_handler(content_types=['contact'])
 def contact(message):
+    print(message.contact)
     if message.contact is not None and getUser(message.chat.id) is None:
         phone = message.contact.phone_number
-        telebot.types.ReplyKeyboardRemove()
-        msg = bot.send_message(message.chat.id, 'Напишите Фамилию и Имя:\nПример: Юлдашев Нуриддин')
+        msg = bot.send_message(message.chat.id, 'Напишите Фамилию и Имя:\nПример: Юлдашев Нуриддин', reply_markup=telebot.types.ReplyKeyboardRemove())
         bot.register_next_step_handler(msg, getName, phone)
 
 
@@ -72,9 +100,4 @@ def get_doc(message):
                 pass
 
 
-while True:
-    try:
-        bot.polling(none_stop=True, timeout=20)
-    except Exception:
-        bot.stop_bot()
-        time.sleep(60)
+bot.polling()
